5abb724141b108faf01b73dc2b6372e29e365d78
Alyona Cherepanova <alyonacherepanova6954@gmail.com>
2023-03-09 15:10:56 +0300

    Add concurrencyLevel (D)

--- 03.web-service/web-service/go.mod
+++
@@ -1,5 +0,0 @@
-module src/github.com
-
-go 1.18
-
-require github.com/gorilla/mux v1.8.0 // indirect
--- 03.web-service/web-service/src/main.go
+++ 03.web-service/web-service/src/main.go
@@ -5,31 +5,45 @@ import (
 	"io"
 	"net"
 	"os"
+	"strconv"
 	"strings"
 )
 
 const (
-	HOST = "localhost"
-	PORT = "3334"
-	TYPE = "tcp"
+	HOST      = "localhost"
+	PORT      = "3334"
+	TYPE      = "tcp"
+	FILE_SIZE = 1 << 13
 )
 
 func main() {
-	l, err := net.Listen(TYPE, HOST+":"+PORT)
+
+	concurrencyLevel := 1
+	var err error
+
+	if len(os.Args) >= 2 {
+		concurrencyLevel, err = strconv.Atoi(os.Args[1])
+		if err != nil {
+			fmt.Println("Arg must be int:", err.Error())
+			os.Exit(1)
+		}
+	}
+
+	ansListen, err := net.Listen(TYPE, HOST+":"+PORT)
 	if err != nil {
 		fmt.Println("Error listening:", err.Error())
 		os.Exit(1)
 	}
-	defer l.Close()
+	defer ansListen.Close()
 	fmt.Println("Listening on " + HOST + ":" + PORT)
 	for {
-		conn, err := l.Accept()
+		conn, err := ansListen.Accept()
 		if err != nil {
 			fmt.Println("Error accepting: ", err.Error())
 			os.Exit(1)
 		}
-		println()
-		go handleRequest(conn)
+
+		go handleRequest(conn, make(chan bool, concurrencyLevel))
 	}
 }
 
@@ -41,29 +55,30 @@ func getFileData(fileName string) ([]byte, error) {
 		return nil, err
 	}
 	defer f.Close()
-	buf := make([]byte, 1024)
+	buf := make([]byte, FILE_SIZE)
 	for {
 		n, err := f.Read(buf)
-		if err == io.EOF {
+		switch err {
+		case io.EOF:
 			break
-		}
-		if err != nil {
+		case nil:
+			return buf[:n], nil
+		default:
 			fmt.Println(err)
 			continue
 		}
-		if n > 0 {
-			return buf[:n], nil
-		}
 	}
-
-	return nil, nil
 }
 
-func handleRequest(conn net.Conn) {
+func handleRequest(conn net.Conn, sem chan bool) {
 
-	defer conn.Close()
+	sem <- true
+	defer func() {
+		<-sem
+		conn.Close()
+	}()
 
-	buf := make([]byte, 1024)
+	buf := make([]byte, FILE_SIZE)
 	reqLen, err := conn.Read(buf)
 
 	if err != nil {
